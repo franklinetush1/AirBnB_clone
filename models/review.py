e9889ca5cee4feed3acd15af06f633d4756f50a5
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-12 21:57:23 +0300

    Update review.py

---
+++ models/review.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines review class"""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Reviews from users"""
+    place_id = ""
+    user_id = ""
+    text = ""
