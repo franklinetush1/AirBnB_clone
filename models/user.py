2746e79c9db303885ba641a3fdfb2cd05e5d77e6
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-12 21:59:29 +0300

    Update user.py

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Creates a User class"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """User details"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
