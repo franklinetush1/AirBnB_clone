9f1af511577d9ce47b82b77c9eccd71f1bda913f
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-13 11:41:43 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,13 @@
 import json
 from models.base_model import BaseModel
 import os
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
 class FileStorage:
     """A class that performs serialization and deserialization"""
     __file_path = "file.json"
