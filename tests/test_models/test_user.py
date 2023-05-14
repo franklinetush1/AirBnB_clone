0762ae773619bb793778ace2feb32089fb4c0d62
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-14 11:09:32 +0300

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,15 +8,15 @@ import datetime
 class UserCase(unittest.TestCase):
     """Tests instances and methods from user class"""
 
-    usr = User()
+    usr1 = User()
 
     def test_class_exists(self):
         """tests if class exists"""
-        self.assertEqual(str(type(self.usr)), "<class 'models.user.User'>")
+        self.assertEqual(str(type(self.usr1)), "<class 'models.user.User'>")
 
     def test_user_inheritance(self):
         """test if User is a subclass of BaseModel"""
-        self.assertIsInstance(self.usr, User)
+        self.assertIsInstance(self.usr1, User)
 	
 	def test_new_instance_stored_in_objects(self):
         self.assertIn(User(), models.storage.all().values())
@@ -29,15 +29,15 @@ class UserCase(unittest.TestCase):
 	
 	def test_save(self):
         sleep(0.05)
-        first_updated_at = usr.updated_at
-        usr.save()
-        self.assertLess(first_updated_at, usr.updated_at)
+        first_updated_at = usr1.updated_at
+        usr1.save()
+        self.assertLess(first_updated_at, usr1.updated_at)
 	
 	def test_to_dict_contains_correct_keys(self):
-        self.assertIn("id", usr.to_dict())
-        self.assertIn("created_at", usr.to_dict())
-        self.assertIn("updated_at", usr.to_dict())
-        self.assertIn("__class__", usr.to_dict())	
+        self.assertIn("id", usr1.to_dict())
+        self.assertIn("created_at", usr1.to_dict())
+        self.assertIn("updated_at", usr1.to_dict())
+        self.assertIn("__class__", usr1.to_dict())	
 	
 if __name__ == '__main__':
     unittest.main()
